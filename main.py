7b97afb61be2f246aae96a0d9767bfb57dc29c7b
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-02-14 13:59:11 +0300

    Практическая №1 14.02.2023
    Александрова ИБС 235

---
+++ main.py
@@ -0,0 +1,10 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+big_netseloe = tseloe*3.5
+print(float(drobnoe)-1.0)
+print(big_netseloe)
+print( tseloe / drobnoe)
+print(big_netseloe/ drobnoe)
+stroka = stroka*2+"Yes"*3
+print(stroka)
