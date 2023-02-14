e965f0f8a6793ceac9c1b93db043ca718eccabf9
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-02-14 14:29:17 +0300

    Практическая №2 14.02.2023
    Александрова

---
+++ main (1).py
@@ -0,0 +1,23 @@
+w = 2
+r = 6
+print((w<r)and(r<w))
+print((w>r)and(r<w))
+print((w<r)and(r>w))
+print((w<r)and(r<w + 6))
+print ("переменные")
+print((w<r)or(r<w))
+print((w<r)or(r>w))
+print((w>r)or(r<w))
+print((w>r)or(98865+r<w))
+print("текст с and")
+t = "Таня"
+u = "Саня"
+print((t==u)and(t==u))
+print((u!=t)and(u!=t))
+print((t!=u)and(t!=u))
+print((u==t)and(u==t))
+print("текст с OR")
+print((t==u)or(t==u))
+print((u!=t)or(u!=t))
+print((t!=u)or(t!=u))
+print((u==t)or(u==t))
