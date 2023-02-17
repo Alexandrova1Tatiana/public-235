d8f562042ef66a9f3837ee0aca2a1f15b1eff20e
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-02-17 09:44:36 +0300

    Практическое задание №2 17.02.2023

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-tseloe = 10
-drobnoe = 8.4
-stroka = "No"
-big_netseloe = tseloe*3.5
-print(float(drobnoe)-1.0)
-print(big_netseloe)
-print( tseloe / drobnoe)
-print(big_netseloe/ drobnoe)
-stroka = stroka*2+"Yes"*3
-print(stroka)
+x = int(input())
+if x > 0:
+    print("1")
+else:
+    print("-1")
