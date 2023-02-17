bb5dbe57a7cbf761056dcc1fd96d6072dcbc39e3
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-02-17 09:52:00 +0300

    Практическое задание №3 17.02.2023

--- main (1).py
+++ main (1).py
@@ -1,23 +1,8 @@
-w = 2
-r = 6
-print((w<r)and(r<w))
-print((w>r)and(r<w))
-print((w<r)and(r>w))
-print((w<r)and(r<w + 6))
-print ("переменные")
-print((w<r)or(r<w))
-print((w<r)or(r>w))
-print((w>r)or(r<w))
-print((w>r)or(98865+r<w))
-print("текст с and")
-t = "Таня"
-u = "Саня"
-print((t==u)and(t==u))
-print((u!=t)and(u!=t))
-print((t!=u)and(t!=u))
-print((u==t)and(u==t))
-print("текст с OR")
-print((t==u)or(t==u))
-print((u!=t)or(u!=t))
-print((t!=u)or(t!=u))
-print((u==t)or(u==t))
+x = int(input("x = "))
+y = int(input("y = "))
+if x > y:
+    print ("x больше y")
+elif x < y:
+    print ("y больше x")
+else:
+    print ("Равны")
