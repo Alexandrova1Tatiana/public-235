666c64ee2f5eb67d8fe87be347cb54e1da22e596
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-03-03 09:46:52 +0300

    задание 14 03.03.2023

--- main (4).py
+++ main (4).py
@@ -1,11 +1,3 @@
-w = 6
-r = 7
-if w > r: 
-    u = w - r 
-elif w < r: 
-        u = w + r 
-else: u == w 
-p=u+w
-if p>0:
-    print(p)
-    
+n =int(input("Площадь"))
+m = int(input("Численность населения"))
+print("Плотность населения",m/n)
