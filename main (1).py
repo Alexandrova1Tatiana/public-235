87867fdce95e1d70bd31dcb703ce49a98cc8da89
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-03-03 09:36:06 +0300

    задание 11 03.03.2023

--- main (1).py
+++ main (1).py
@@ -1,8 +1,3 @@
-x = int(input("x = "))
-y = int(input("y = "))
-if x > y:
-    print ("x больше y")
-elif x < y:
-    print ("y больше x")
-else:
-    print ("Равны")
+n =int(input("Ввод обьема"))
+m = int(input("Ввод массы"))
+print(m/n)
