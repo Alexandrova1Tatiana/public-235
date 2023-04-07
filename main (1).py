085cf256d059fd58bff616198e472e187c434cf9
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-04-07 09:12:33 +0300

    Домашнее задание №2

--- main (1).py
+++ main (1).py
@@ -1,15 +1,5 @@
-z=int(input("Введите число дня:"))
-if 1<=z<=31:
-    e = int(input("Введите пробег:"))
-    if e<1000 and e>99:
-        r = e//100
-        t = e%100
-        y = t//10
-        u = t%10
-        k= u+y+r
-        if k>=z:
-            print("Сброс")
-        else:
-            print("Сегодня не сломался")
-else:
-    print("Ошибка")
+q=1
+w=64
+while q<=20:
+    print (w**q)
+q=1
