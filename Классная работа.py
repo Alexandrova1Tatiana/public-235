6b17c62d55fc2496d26f82c8b0c4e14568b149f0
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-04-28 09:19:05 +0300

    задача 7

--- Классная работа.py
+++ Классная работа.py
@@ -1,4 +1,10 @@
-x= 600
-while x %28 1= 0:
-    x -= 1
-print (X)
+a = int (input( 'введи число: "))
+n = 1
+while True:
+    s = 0
+for i in range(1, n + 1):
+    S += 1 / i
+if s > a:
+    print('1....', n - 1, sep='")
+    break
+n += 1
