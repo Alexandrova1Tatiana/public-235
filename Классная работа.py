fe05a72c33bea5cd4e4d7ac7d1e38a1fe848213d
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-04-28 09:24:58 +0300

    задача 9

--- Классная работа.py
+++ Классная работа.py
@@ -1,12 +1,4 @@
-E = 0.0001
-
-s = 0
-n = 1
-while True:
-    delta = (2*n-1)/2*n
-    s += delta
-if delta < E:
-    break
-n += 1
-print(delta)
-print(s)
+x = 201
+while x%17 != 0:
+    x += 1
+print(x)
