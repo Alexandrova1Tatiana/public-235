8af7db17d10e8d1a4d3f06244c3a6661b7438107
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-05-05 07:21:13 +0300

    домашняя работа FOR

--- Классная работа.py
+++ Классная работа.py
@@ -1,10 +1,16 @@
-a=0
-n=10
-for i in range(1,n+ 1,2):
-    a=a+1
-    if a==5:
-        continue
-    print (a)
-    if a==7:
+from random import random
+n = round(random() * 100)
+i = 1
+print("Компьютер загадал число. Отгадайте его. У вас 100 попыток")
+while i <= 100:
+    u = int(input(str(i) + '-я попытка: '))
+    if u > n:
+        print('Много')
+    elif u < n:
+        print('Мало')
+    else:
+        print('Вы угадали с %d-й попытки' % i)
         break
-print ("всё!")
+    i += 1
+else:
+    print('Вы исчерпали 10 попыток. Было загадано', n)
