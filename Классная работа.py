f515fe080ab8ca8b0040ad7f6014c395ce5e6346
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-05-11 21:55:59 +0300

    задание 1

--- Классная работа.py
+++ Классная работа.py
@@ -1,16 +1,5 @@
-from random import random
-n = round(random() * 100)
-i = 1
-print("Компьютер загадал число. Отгадайте его. У вас 100 попыток")
-while i <= 100:
-    u = int(input(str(i) + '-я попытка: '))
-    if u > n:
-        print('Много')
-    elif u < n:
-        print('Мало')
-    else:
-        print('Вы угадали с %d-й попытки' % i)
-        break
-    i += 1
-else:
-    print('Вы исчерпали 10 попыток. Было загадано', n)
+x = 25,4
+q= 10
+for x in range (10,22,2):
+    print(q*x)
+    q+=x
