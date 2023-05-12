e4bcf00be6d142c09cd6076bbb27b7630580a12c
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-05-12 10:14:37 +0300

    задача 5 отредактированная

--- Классная работа.py
+++ Классная работа.py
@@ -1,7 +1,4 @@
-a=int (input ("Введите а:"))
-b=int (input ("Введите b:"))
-x=a
-for x in range (a,b+1) :
-    y= x**(1/2)
-    print("Ответ :",x,y)
-    x+= 1
+x = 21/10
+for x in range (21,30,1) :
+    print (x/10)
+    x += 1/100
