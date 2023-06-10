fc1c917b980008dc0a643931af9dc08105c1eaeb
Александрова Татьяна <124045569+Alexandrova1Tatiana@users.noreply.github.com>
2023-06-10 13:18:14 +0300

    Задача 3 (Столетия)
    Первое столетие охватывает период с 1 года по 100 год включительно , второе столетие — с 101 года по 200 год включительно и т. д.
    
    Задача
    Учитывая год, верните столетие, в котором он находится.

--- 555.py
+++ 555.py
@@ -1,29 +1,7 @@
-def date(day, month, year):
-
-   if day <= 0 or month <= 0 or year < 0:
-
-       return False
-
-   else:
-
-       months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-       if year % 4 == 0:  months[1] = 29
-
-       if day <= months[month - 1]:
-
-           if month <= 12:
-
-               return True
-
-       return False
-
-if __name__ == '__main__':
-
-   day = int(input('Day: '))
-
-   month = int(input('Month: '))
-
-   year = int(input('Year: '))
-
-   print(date(day, month, year))
+year= int(input("Введите год:"))
+def century(year):
+    if year % 10 == 0:
+        return (year // 100)
+    else:
+        return (year // 100) +1
+print(century(year))
